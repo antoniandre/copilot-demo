3870b86bba7f62bbd4cef596f5f2aeb5b9d9d915
antoniandre <antoniandre.web@gmail.com>
2024-09-06 13:05:44 +0200

    Add the basic structure and styles. TBC!

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,58 @@
 </script>
 
 <template lang="pug">
+  .top-bar
+    .logo
+    nav
+      ul
+        li
+          a Product
+        li
+          a Solutions
+        li
+          a Resources
+        li
+          a Open Source
+        li
+          a Enterprise
+        li
+          a Pricing
+
   header
+    section.section.section--hero
+      h1 The world’s most widely adopted AI developer tool.
+
   main
+    section.section.section--enterprise-ready
+      .pill Enterprise Ready
+      h2 The competitive advantage developers ask for by name.
+
+    section.section.section--industry-standard
+      h2 The Industry Standard.
+
+    section.section.section--features-focus
+      .pill Features
+      h2 The AI coding assistant elevating developer workflows.
+
+    section.section.section--pricing
+      .pill Pricing
+      h2 Take flight with GitHub Copilot.
+
+    section.section.section--features-table
+      h2 Features Table
+
+    section.section.section--need-human
+      h2 Need Human
+
+    section.section.section--related
+      h2 Get the most out of GitHub Copilot.
+
+    section.section.section--faq
+      h2 FAQ
+
   footer
+    .logo Github
+    nav.menu Menu
 </template>
 
 <style scoped>
