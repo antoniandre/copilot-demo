db4f0bce020e9a21c339357482bfeb2a9fbe73da
antoniandre <antoniandre.web@gmail.com>
2024-09-13 13:03:35 +0200

    Added font, hero video and texts.

--- src/App.vue
+++ src/App.vue
@@ -2,58 +2,70 @@
 </script>
 
 <template lang="pug">
-  .top-bar
-    .logo
-    nav
-      ul
-        li
-          a Product
-        li
-          a Solutions
-        li
-          a Resources
-        li
-          a Open Source
-        li
-          a Enterprise
-        li
-          a Pricing
+.top-bar
+  .logo
+  nav
+    ul
+      li
+        a Product
+      li
+        a Solutions
+      li
+        a Resources
+      li
+        a Open Source
+      li
+        a Enterprise
+      li
+        a Pricing
 
-  header
-    section.section.section--hero
-      h1 The world’s most widely adopted AI developer tool.
+header
+  section.section.section--hero
+    .pill.pill--rainbow Copilot Enterprise now available
+    h1 The world’s most widely adopted AI developer tool.
+    video(
+      autoplay=""
+      playsinline=""
+      muted=""
+      width="1248"
+      height="735"
+      poster="https://github.githubassets.com/assets/hero-poster-18f705106687.webp")
+      source(src="https://github.githubassets.com/assets/hero-lg-6a98e47708e8.mp4"
+      type="video/mp4; codecs=avc1.4d002a")
 
-  main
-    section.section.section--enterprise-ready
-      .pill Enterprise Ready
-      h2 The competitive advantage developers ask for by name.
+main
+  section.section.section--enterprise-ready
+    .pill Enterprise Ready
+    h2 The competitive advantage developers ask for by name.
 
-    section.section.section--industry-standard
-      h2 The Industry Standard.
+  section.section.section--industry-standard
+    h2 The Industry Standard.
+    .head
 
-    section.section.section--features-focus
-      .pill Features
-      h2 The AI coding assistant elevating developer workflows.
+  section.section.section--features-focus
+    .pill Features
+    h2 The AI coding assistant elevating developer workflows.
 
-    section.section.section--pricing
-      .pill Pricing
-      h2 Take flight with GitHub Copilot.
+  section.section.section--pricing
+    .pill Pricing
+    h2 Take flight with GitHub Copilot.
 
-    section.section.section--features-table
-      h2 Features Table
+  section.section.section--features-table
+    h2 Features Table
 
-    section.section.section--need-human
-      h2 Need Human
+  section.section.section--help
+    h2 Need human help?
+    p Let’s define how to propel your team into a new era.
 
-    section.section.section--related
-      h2 Get the most out of GitHub Copilot.
+  section.section.section--related
+    h2 Get the most out of GitHub Copilot.
 
-    section.section.section--faq
-      h2 FAQ
+  section.section.section--faq
+    h2 FAQ
 
-  footer
-    .logo Github
-    nav.menu Menu
+footer
+  .logo Github
+  nav.menu Menu
 </template>
 
 <style scoped>
